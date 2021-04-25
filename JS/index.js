d6c28c6fa92f3322355eddfc2eef30e374755aed
Jeongjin Kim <kjzon@hotmail.com>
2021-04-25 22:34:08 +0900

    done with basic functionality

--- JS/index.js
+++ JS/index.js
@@ -6,20 +6,63 @@ const amount = document.getElementById("jsAmount");
 const list = document.getElementById("jsList");
 const allAmount = document.querySelectorAll(".list__amount");
 const totalAmount = document.getElementById("jsTotalamount");
+const delButton = document.querySelectorAll("button");
 
 let total = 0;
+let idNum = 1;
 
+//key
 let EXPENSE_LS = "expense";
+
+//value
 let expense = [];
 
 const postTotalAmount = (sum) => {
   total += parseInt(sum, 10);
-  console.log(total, parseInt(sum, 10));
   totalAmount.innerText = `₩${total}`;
+};
+
+const removeExpense = (e) => {
+  //to target clicked button
+  const btn = e.target;
+  //to get the parentNode of clicked button
+  li = btn.parentNode;
+  const removedAmount = li.children[2].innerText;
+
+  list.removeChild(li);
+
+  //remove from the Localstorage
+  const filteredExpense = expense.filter((item) => {
+    //return if id value of expenseObj inside expense array
+    //is not equal to li(btn.parentNode)'s id
+
+    return item.id != li.id;
+  });
+
+  //destract from total
+  total = total - removedAmount;
+  totalAmount.innerText = `₩${total}`;
+
+  //update new expense array
+  expense = filteredExpense;
+  saveExpense();
 };
 
 const saveExpense = () => {
   localStorage.setItem(EXPENSE_LS, JSON.stringify(expense));
+};
+
+const loadExpense = () => {
+  //load value from local storage(as a string)
+  const loadedExpense = localStorage.getItem(EXPENSE_LS);
+  //parse stringifies value to object
+  const parsedExpense = JSON.parse(loadedExpense);
+  if (loadedExpense != null) {
+    parsedExpense.forEach(function (expenseItem) {
+      addExpense(expenseItem.date, expenseItem.description, expenseItem.amount);
+      postTotalAmount(expenseItem.amount);
+    });
+  }
 };
 
 const addExpense = (dat, desc, amnt) => {
@@ -27,20 +70,30 @@ const addExpense = (dat, desc, amnt) => {
   const divDate = document.createElement("div");
   const divDesc = document.createElement("div");
   const divAmnt = document.createElement("div");
+  const delBtn = document.createElement("button");
   divDate.classList.add("list__date");
   divDesc.classList.add("list__description");
   divAmnt.classList.add("list__amount");
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", removeExpense);
+
   divDate.innerText = dat;
   divDesc.innerText = desc;
   divAmnt.innerText = amnt;
+
   li.appendChild(divDate);
   li.appendChild(divDesc);
   li.appendChild(divAmnt);
+  li.appendChild(delBtn);
+
+  const newId = idNum++;
+  li.id = newId;
   list.prepend(li);
   const expenseObj = {
     date: dat,
     description: desc,
     amount: amnt,
+    id: newId,
   };
   expense.push(expenseObj);
   saveExpense();
@@ -53,10 +106,14 @@ const handleSubmit = (e) => {
   const amnt = amount.value;
   addExpense(dat, text, amnt);
   postTotalAmount(amnt);
+  amount.value = "0";
 };
 
 function init() {
   form.addEventListener("submit", handleSubmit);
+
+  loadExpense();
+  console.log(delButton);
 }
 
 init();
